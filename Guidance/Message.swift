172f4954df89bfca4ec9dd259fbef986871bf835
Dzung Le <morepositive@outlook.com>
2017-11-23 13:29:29 +0700

    changed text to simple 2d image for improved performance and visuals

--- Guidance/Message.swift
+++ Guidance/Message.swift
@@ -36,24 +36,55 @@ class Message: LocationNode {
     
     func loadMessageAndBackground(message: String, atPoint point: SCNVector3) {
         
-        let message = SCNText(string: message, extrusionDepth: 0)
-        message.containerFrame = CGRect(origin: .zero, size: CGSize(width: 100.0, height: 500.0))
-        message.isWrapped = true
+//        let message = SCNText(string: message, extrusionDepth: 0)
+//        message.containerFrame = CGRect(origin: .zero, size: CGSize(width: 100.0, height: 500.0))
+//        message.isWrapped = true
+//
+//        message.font = UIFont(name: "San Francisco", size: 16)
+//        message.flatness = 1
+//        let messageNode = SCNNode(geometry: message)
+//        center(node: messageNode)
+//
+        let messageView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 500.0, height: 500.0)))
+        let messageLabel = EdgeInsetLabel(frame: CGRect(origin: .zero, size: CGSize(width: 500.0, height: 500.0)))
         
-        message.font = UIFont(name: "San Francisco", size: 16)
-        message.flatness = 1
-        let messageNode = SCNNode(geometry: message)
-        center(node: messageNode)
-        let (minVec, maxVec) = messageNode.boundingBox
+        messageView.backgroundColor = UIColor.blue.withAlphaComponent(0.75)
         
-        let borderWidth = CGFloat(maxVec.x - minVec.x) + 10
-        let borderHeight = CGFloat(maxVec.y - minVec.y) + 10
+        messageLabel.textInsets = UIEdgeInsetsMake(10, 20, 10, 20)
         
-        let backgroundBox = SCNPlane(width: borderWidth, height: borderHeight)
-        backgroundBox.cornerRadius = borderWidth / 10
-        backgroundBox.firstMaterial!.diffuse.contents = UIColor.blue.withAlphaComponent(1)
-        backgroundBox.firstMaterial!.isDoubleSided = true
+        messageLabel.numberOfLines = 0
+        messageLabel.text = message
+        messageLabel.textAlignment = .center
+        
+        let sizeToFit = CGSize(width: messageLabel.frame.size.width, height: CGFloat.greatestFiniteMagnitude)
+        let textSize = messageLabel.sizeThatFits(sizeToFit)
+        
+        let messageViewOrigin = CGPoint(x: 10, y: (messageView.frame.size.height - textSize.height) / 2)
+       
+        messageView.frame = CGRect(origin: messageViewOrigin, size: CGSize(width: textSize.width, height: textSize.height))
+        
+        let backside = generateImageFromView(inputView: messageView)
+        
+        messageView.addSubview(messageLabel)
+        messageLabel.sizeToFit()
+        
+        messageLabel.textColor = UIColor.white
+        messageLabel.font = UIFont(name: "San Francisco", size: 16)
+        
+        let textImage = generateImageFromView(inputView: messageView)
+        
+        
+        let backgroundBox = SCNPlane(width: textImage.size.width / 100, height: textImage.size.height / 100)
+        backgroundBox.firstMaterial!.diffuse.contents = textImage
+        backgroundBox.cornerRadius = 0.1
+        
+        let backsideBackgroundBox = SCNPlane(width: textImage.size.width / 100, height: textImage.size.height / 100)
+        backsideBackgroundBox.firstMaterial!.diffuse.contents = backside
+        backsideBackgroundBox.firstMaterial!.isDoubleSided = true
+        backsideBackgroundBox.cornerRadius = 0.1
+        
         let boxNode = SCNNode(geometry: backgroundBox)
+        let backsideNode = SCNNode(geometry: backsideBackgroundBox)
         
         // Make text face camera.
 //        guard let sceneViewOrientation = sceneView.pointOfView?.orientation else { return SCNNode() }
@@ -61,10 +92,11 @@ class Message: LocationNode {
 //        self.orientation = sceneViewOrientation
         
         self.addChildNode(boxNode)
-        self.addChildNode(messageNode)
+        self.addChildNode(backsideNode)
+        
         
         self.position = point
-        self.scale = SCNVector3(0.005, 0.005, 0.005)
+//        self.scale = SCNVector3(0.005, 0.005, 0.005)
         
     }
     
@@ -120,6 +152,26 @@ class Message: LocationNode {
         let uiImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         return uiImage
+    }
+}
+
+class EdgeInsetLabel: UILabel {
+    var textInsets = UIEdgeInsets.zero {
+        didSet { invalidateIntrinsicContentSize() }
+    }
+    
+    override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        let insetRect = UIEdgeInsetsInsetRect(bounds, textInsets)
+        let textRect = super.textRect(forBounds: insetRect, limitedToNumberOfLines: numberOfLines)
+        let invertedInsets = UIEdgeInsets(top: -textInsets.top,
+                                          left: -textInsets.left,
+                                          bottom: -textInsets.bottom,
+                                          right: -textInsets.right)
+        return UIEdgeInsetsInsetRect(textRect, invertedInsets)
+    }
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: UIEdgeInsetsInsetRect(rect, textInsets))
     }
 }
 
